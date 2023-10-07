72d2ef7af285f7dcd5a4cab9539c66285da742d2
yehya <yehyazakareya3@gmail.com>
2023-10-07 19:42:49 +0300

    first deployment

--- app.py
+++ app.py
@@ -33,6 +33,24 @@ class LoginVo(db.Model):
     loginUsername = db.Column('username',db.String,nullable=False)
     loginPassword = db.Column('hash',db.String(100),nullable=False)
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 with app.app_context():
     db.create_all()
 
